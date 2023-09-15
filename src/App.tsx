587951d4fa48ddae4f0bd61fe84c79b862cbdd85
nanidam <nani.dam@medieinstitutet.se>
2023-09-15 16:43:32 +0200

    ➕ add enriched occupation service & test.rest

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { DigiButton } from "@digi/arbetsformedlingen-react";
 import { ButtonVariation } from "@digi/arbetsformedlingen";
 
 export function App() {
-  function myFunction(e: any) {
+  function myFunction(e: unknown) {
     console.log(e);
   }
 
