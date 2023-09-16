e1248c85d69ca4a5d63b21d3499e47f6ba6dd6fb
jenmwa <jenmwa@gmail.com>
2023-09-16 21:54:04 +0200

    add service & interface for jobtech/yrkesprognos

---
+++ src/models/ICurrentOccupationalForecast.ts
@@ -0,0 +1,25 @@
+export interface ICurrentOccupationalForecast {
+  concept_id: string,
+  ar: string,
+  bristvarde: number,
+  geografi: string,
+  ssyk: number,
+  ingress: string,
+  stycke1: string,
+  stycke2: string,
+
+
+}
+
+
+// {
+//   "concept_id": "bZ4J_riZ_zK6",
+//   "ar": 23,
+//   "bristvarde": 4.0,
+//   "datatyp": "Bristindex",
+//   "geografi": "Riket",
+//   "ssyk": 7114,
+//   "ingress": " Det finns cirka 18000 yrkesverksamma anläggningsarbetare i Sverige, varav cirka 3 procent är kvinnor och 97 procent är män.",
+//   "stycke1": "Arbetsförmedlingen bedömer att det kommer vara liten konkurrens om jobben för anläggningsarbetare under det närmaste året. Det innebär att personer som har den utbildning eller erfarenhet som krävs kommer ha stora möjligheter till arbete, men beroende på inriktning inom yrket kan arbetsmarknaden variera.",
+//   "stycke2": "På 3 års sikt bedömer Arbetsförmedlingen att möjligheterna till arbete för anläggningsarbetare kommer vara stora. Möjligheterna till arbete inom yrket ökar om du har relevanta certifikat, behörigheter eller yrkesbevis."
+// },
