c30d4a7c77010a86daf35ec1a42070ffcde26954
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-18 11:00:13 +0200

    one gets data when submitting form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,20 +1,60 @@
 import { ButtonVariation, FormInputType, FormInputValidation, FormInputVariation, FormTextareaValidation, FormTextareaVariation } from "@digi/arbetsformedlingen";
 import { DigiButton, DigiFormInput, DigiFormTextarea } from "@digi/arbetsformedlingen-react";
 import { FormEvent, useState } from "react";
+import { matchByText } from "../services/matchByTextServices";
+import { ISearchByText } from "../models/ISearchByText";
+//import { WithContext as ReactTags } from 'react-tag-input';
+
+// interface ITags {
+//   id: string,
+//   text: string
+// }
 
 export default function SearchForm() {
   const [freeSearch, setFreeSearch] = useState<string>('');
-  const [headerSearch, setHeaderSearch] = useState<string | number>('');
+  const [headerSearch, setHeaderSearch] = useState<string>('');
+  // const [tags, setTags] = useState<ITags[]>([]);
 
-  const getWorkTitles = (e: FormEvent) => {
+  // const handleDelete = i => {
+  //   setTags(tags.filter((tag, index) => index !== i));
+  // };
+
+  // const handleAddition = tag => {
+  //   setTags([...tags, tag]);
+  //   console.log(tags)
+  // };
+
+  const getWorkTitles = async (e: FormEvent) => {
     e.preventDefault();
-    console.log(freeSearch, headerSearch)
+    let search: ISearchByText
+
+     if (headerSearch === '') {
+      search = {
+      input_text: freeSearch
+      }
+    } else {
+      search = {
+      input_text: freeSearch,
+      input_headline: headerSearch,
+      }
+    }
+
+    const data = await matchByText(search);
+    console.log(freeSearch, headerSearch, data)
   }
 
   return(
     <section>
       <h3>Sök yrken</h3>
       <form onSubmit={(e: FormEvent) => getWorkTitles(e)}>
+        {/* <h2>Ange minst tre saker du kommer lära dig på utbildningen</h2> */}
+        {/* <ReactTags
+        tags={tags}
+        handleDelete={handleDelete}
+        handleAddition={handleAddition}
+        inputFieldPosition="bottom"
+        placeholder="tryck 'enter' för att lägga till"
+        required={true}/> */}
         <DigiFormTextarea
           afLabel="Vad innehåller utbildningen du är intresserad av?"
           afLabelDescription="Ange minst tre saker du kommer lära dig på utbildningen"
@@ -30,7 +70,7 @@ export default function SearchForm() {
           afType={FormInputType.TEXT}
           afValidation={FormInputValidation.NEUTRAL}		
           afRequired={false}	
-          onAfOnChange={(e) => setHeaderSearch(e.target.value)}
+          onAfOnChange={(e) => setHeaderSearch(JSON.stringify(e.target.value))}
         >
         </DigiFormInput>
         <DigiButton afType="submit" afVariation={ButtonVariation.PRIMARY}>
