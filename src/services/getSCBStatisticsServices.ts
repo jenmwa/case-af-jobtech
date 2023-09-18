3001527a86c7a57870d992e6741c3ba7bcd472ac
jenmwa <jenmwa@gmail.com>
2023-09-18 21:10:43 +0200

    cleanup

--- src/services/getSCBStatisticsServices.ts
+++ src/services/getSCBStatisticsServices.ts
@@ -1,53 +1,51 @@
 import axios from "axios";
 import { IGetSCBStatisticsSalary, IGetSCBStatisticsSalaryResponse } from "../models/IGetSCBStatisticsSalary";
 
-
 const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 const URL = 'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0110/AM0110A/LonYrkeRegion4A';
-const SSYKcode = "2512";
-const REQUESTBODY: IGetSCBStatisticsSalary = {
 
-  "query": [
-    {
-      "code": "Yrke2012",
-      "selection": {
-        "filter": "item",
-        "values": [
-          SSYKcode
-        ]
+export const getSCBStatisticsSalary = async (ssyk: string) => {
+
+  const REQUESTBODY: IGetSCBStatisticsSalary = {
+    "query": [
+      {
+        "code": "Yrke2012",
+        "selection": {
+          "filter": "item",
+          "values": [
+            ssyk
+          ]
+        }
+      },
+      {
+        "code": "ContentsCode",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "000000BW"
+          ]
+        }
+      },
+      {
+        "code": "Tid",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022"
+          ]
+        }
       }
-    },
-    {
-      "code": "ContentsCode",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "000000BW"
-        ]
-      }
-    },
-    {
-      "code": "Tid",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "2016",
-          "2017",
-          "2018",
-          "2019",
-          "2020",
-          "2021",
-          "2022"
-        ]
-      }
+    ],
+    "response": {
+      "format": "json"
     }
-  ],
-  "response": {
-    "format": "json"
   }
-}
-
-export const getSCBStatisticsSalary = async () => {
   try {
     const response = await axios.post<IGetSCBStatisticsSalaryResponse>(`${PROXY_URL}${URL}`, REQUESTBODY);
     console.log(response.data);
