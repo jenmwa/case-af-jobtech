e7055b7b25f3e1e6cedc724779e273dee7b612f3
jenmwa <jenmwa@gmail.com>
2023-09-19 20:49:53 +0200

    add AVG for both 2023 & 2026

--- src/components/OccupationForecast.tsx
+++ src/components/OccupationForecast.tsx
@@ -11,32 +11,25 @@ export const OccupationForecast = ({
 }: IOccupationForecast) => {
   console.log(deficiencyValue2023);
 
-  const checkDeficiencyValues = () => {
-    const bristvarde2023 = Number(deficiencyValue2023?.bristvarde);
-    const bristvarde2026 = Number(deficiencyValue2026?.bristvarde);
-
-    if (bristvarde2023 <= 2 || bristvarde2026 <= 2) {
-      console.log("no need");
+  const checkDeficiencyValues = (bristvarde: number) => {
+    if (bristvarde <= 2) {
       return { value: "400", text: "Ej brist" };
-      // return "400";
-    } else if (
-      bristvarde2023 === 3 ||
-      bristvarde2026 === 3 ||
-      bristvarde2023 === 4 ||
-      bristvarde2026 === 4
-    ) {
-      console.log("looking stable");
-      // setStrokeDashoffsetValue("250");
+    } else if (bristvarde === 3 || bristvarde === 4) {
       return { value: "250", text: "I balans" };
-    } else if (bristvarde2023 >= 5 || bristvarde2026 >= 5) {
-      console.log("ALL THE NEED!");
+    } else if (bristvarde >= 5) {
       return { value: "60", text: "Hög" };
-      // return "60";
+    } else {
+      return { value: "0", text: "Unknown" };
     }
   };
 
-  const result = checkDeficiencyValues();
-  const htmlSVG = (
+  const bristvarde2023 = Number(deficiencyValue2023?.bristvarde);
+  const bristvarde2026 = Number(deficiencyValue2026?.bristvarde);
+
+  const result2023 = checkDeficiencyValues(bristvarde2023);
+  const result2026 = checkDeficiencyValues(bristvarde2026);
+
+  const htmlSVG2023 = (
     <div>
       <h4>2023</h4>
       <svg width="200" height="200">
@@ -59,11 +52,44 @@ export const OccupationForecast = ({
             stroke="#ca2c92"
             strokeWidth="2rem"
             strokeDasharray="500"
-            strokeDashoffset={result?.value}
+            strokeDashoffset={result2023?.value}
           ></circle>
         </g>
         <text x="50%" y="50%" dominantBaseline="central" textAnchor="middle">
-          {result?.text}
+          {result2023?.text}
+        </text>
+      </svg>
+    </div>
+  );
+
+  const htmlSVG2026 = (
+    <div>
+      <h4>2026</h4>
+      <svg width="200" height="200">
+        <g transform="rotate(-190 100 100)">
+          <circle
+            r="70"
+            cx="100"
+            cy="100"
+            fill="transparent"
+            stroke="lightgrey"
+            strokeWidth="2rem"
+            strokeDasharray="439.8"
+            strokeDashoffset="0"
+          ></circle>
+          <circle
+            r="70"
+            cx="100"
+            cy="100"
+            fill="transparent"
+            stroke="#ca2c92"
+            strokeWidth="2rem"
+            strokeDasharray="500"
+            strokeDashoffset={result2026?.value}
+          ></circle>
+        </g>
+        <text x="50%" y="50%" dominantBaseline="central" textAnchor="middle">
+          {result2026?.text}
         </text>
       </svg>
     </div>
@@ -72,51 +98,9 @@ export const OccupationForecast = ({
   return (
     <>
       <p>Framtidsprognos</p>
-      <p>
-        Efterfrågan 2023:
-        {/* {checkDeficiencyValues()} */}
-      </p>
-      <p>
-        bristvärde 2026:
-        {/* {checkDeficiencyValues()} */}
-      </p>
-      {htmlSVG}
+      {htmlSVG2023}
+      <p>Framtidsprognos</p>
+      {htmlSVG2026}
     </>
   );
 };
-
-// <div>
-// <h4>2023</h4>
-// <svg width="200" height="200">
-//   <g transform="rotate(-190 100 100)">
-//     <circle
-//       r="70"
-//       cx="100"
-//       cy="100"
-//       fill="transparent"
-//       stroke="lightgrey"
-//       stroke-width="2rem"
-//       stroke-dasharray="439.8"
-//       stroke-dashoffset="0"
-//     ></circle>
-//     <circle
-//       r="70"
-//       cx="100"
-//       cy="100"
-//       fill="transparent"
-//       stroke="#ca2c92"
-//       stroke-width="2rem"
-//       stroke-dasharray="500"
-//       stroke-dashoffset={strokeDashoffsetValue}
-//     ></circle>
-//   </g>
-//   {/* <text
-//     x="50%"
-//     y="50%"
-//     dominant-baseline="central"
-//     text-anchor="middle"
-//   >
-//     I balans
-//   </text> */}
-// </svg>
-// </div>
