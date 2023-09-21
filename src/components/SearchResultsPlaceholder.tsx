61ac683ee6e5ee40f3a8cbc857ade046b5a0dea7
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-21 13:24:51 +0200

    added placeholder-illustration

--- src/components/SearchResultsPlaceholder.tsx
+++ src/components/SearchResultsPlaceholder.tsx
@@ -1,3 +1,17 @@
+import { DigiMediaImage } from '@digi/arbetsformedlingen-react'
+import illustration from '/af-illustration.png'
+
 export default function SearchResultsPlaceholder() {
-  return <p>placeholder</p>
+  return (
+    <div className='placeholder'>
+      <DigiMediaImage
+      afUnlazy
+      afHeight="300"
+      afWidth="300"
+      afSrc={illustration}
+      afAlt="Illustratino föreställande glad kvinna"
+    >
+    </DigiMediaImage>
+    </div>
+  )
 }
--- src/style/Wrappers.tsx
+++ src/style/Wrappers.tsx
@@ -4,5 +4,5 @@ export const MainFlex = styled.div`
 @media (min-width: 768px) {
         display: grid;
         grid-template-columns: 1fr 2fr;
-        gap: 30px;
+        gap: 50px;
     }`
