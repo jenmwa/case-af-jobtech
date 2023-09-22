be2233f2c8660ab22e22112b3f7a9c58498268bb
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-22 10:37:50 +0200

    added homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
 import { DigiButton, DigiLayoutContainer } from "@digi/arbetsformedlingen-react";
 import { Link } from "react-router-dom";
+import illustration from '/person.png'
 
 export default function Home() {
   return (
-    <section className="home">
-      <DigiLayoutContainer>
-        <h1>Välkommen till Yrkesvägledarn</h1>
-        <p>Här kan du utforska olika utbildningar och se yrkestitlar du kan axla med efter din utbildning</p>
-        <DigiButton><Link to="sok-utbildning">Sök utbildning</Link></DigiButton>
-        <DigiButton><Link to="sok-yrke">Utforska yrkestitlar</Link></DigiButton>
+    <DigiLayoutContainer>
+      <section className="home">
+        <div className='homeContent'>
+          <h1>Välkommen till Yrkesvägledarn</h1>
+          <p>Här kan du utforska olika utbildningar och se vilak yrkestitlar du kan axla efter din utbildning.</p>
+          <DigiButton><Link to="sok-utbildning">Sök utbildning</Link></DigiButton>
+          <DigiButton><Link to="sok-yrke">Utforska yrkestitlar</Link></DigiButton>
+        </div>
+        <img src={illustration} alt="illustration of woman in front of computer" />
+      </section>
       </DigiLayoutContainer>
-    </section>
   );
 }
