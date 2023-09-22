6ba6ca83bd33d2f2b01c06e91accc51b4cb377ff
nanidam <nani.dam@medieinstitutet.se>
2023-09-22 12:04:22 +0200

    add one digiexpandableaccordion

--- src/components/SearchEducationResults.tsx
+++ src/components/SearchEducationResults.tsx
@@ -1,5 +1,14 @@
+import { DigiExpandableAccordion } from "@digi/arbetsformedlingen-react";
+
 export default function SearchEducationResults() {
   return (
-    <h2>SearchEducationResults</h2>
-  )
+    <>
+      <section>
+        <h2>Sökresultat</h2>
+        <DigiExpandableAccordion afHeading="Utfällbar rubrik">
+          <p>Ea nulla enim enim voluptate mollit proident.</p>
+        </DigiExpandableAccordion>
+      </section>
+    </>
+  );
 }
