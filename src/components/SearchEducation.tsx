171966c5e50cc1520863912e4cd3ff28a83ff9c0
nanidam <nani.dam@medieinstitutet.se>
2023-09-22 10:31:08 +0200

    ✨ added textarea for search edu

--- src/components/SearchEducation.tsx
+++ src/components/SearchEducation.tsx
@@ -1,5 +1,32 @@
+import {
+  FormTextareaVariation,
+  FormTextareaValidation,
+  ButtonSize,
+  ButtonVariation,
+} from "@digi/arbetsformedlingen";
+import { DigiButton, DigiFormTextarea } from "@digi/arbetsformedlingen-react";
+
 export default function SearchEducation() {
   return (
-    <h2>Sök utbildning</h2>
-  )
+    <>
+      <section>
+        <h2>Sök utbildning</h2>
+        <article>
+          <DigiFormTextarea
+            afLabel="Beskriv vad du tycker om att göra"
+            afVariation={FormTextareaVariation.MEDIUM}
+            afValidation={FormTextareaValidation.NEUTRAL}
+            afLabelDescription="Lämnas textrutan blank så hämtas alla utbildningar"
+          ></DigiFormTextarea>
+          <DigiButton
+            afSize={ButtonSize.MEDIUM}
+            afVariation={ButtonVariation.PRIMARY}
+            afFullWidth={false}
+          >
+            Sök utbildning
+          </DigiButton>
+        </article>
+      </section>
+    </>
+  );
 }
