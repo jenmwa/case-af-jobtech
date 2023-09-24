e89fa3d18dfab9f6c0e5f184e2e206b64a9e5157
jenmwa <jenmwa@gmail.com>
2023-09-24 21:58:20 +0200

    font added

--- src/components/styled/Fonts.tsx
+++ src/components/styled/Fonts.tsx
@@ -2,8 +2,7 @@ import { DigiTypography } from "@digi/arbetsformedlingen-react";
 import styled from "styled-components";
 
 export const StyledDigiTypography = styled(DigiTypography).attrs({
-  className: "group9style",
+  className: "group9styling",
 })`
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-family: "Ubuntu", Arial, sans-serif;
 `;
