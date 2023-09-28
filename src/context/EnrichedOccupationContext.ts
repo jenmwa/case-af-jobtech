82cbad5ea90f84ba2594b169f5dc64154bfc488b
jenmwa <jenmwa@gmail.com>
2023-09-28 19:16:23 +0200

    .

--- src/context/EnrichedOccupationContext.ts
+++ src/context/EnrichedOccupationContext.ts
@@ -9,4 +9,10 @@ interface IAction {
 export const EnrichedOccupationContext = createContext<{
   stateEnrichedOccupation: IOccupation[];
   dispatchEnrichedOccupation: React.Dispatch<IAction>;
-}>([]);
+}>({
+  stateEnrichedOccupation: [],
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  dispatchEnrichedOccupation: function (value: IAction): void {
+    throw new Error("Function not implemented.");
+  }
+});
